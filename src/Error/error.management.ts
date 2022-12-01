0ee6b4f590fea590484e927f3b9053a4e5cb2b53
Yanira <yanirazapata.c@gmail.com>
2022-12-01 11:19:52 +0100

    📝  create error management

---
+++ src/Error/error.management.ts
@@ -0,0 +1,49 @@
+import { HTTPError } from "./interfaces/error";
+
+export class UserErrorController {
+
+
+    register {
+        if(req.body.email)
+        //
+
+    }
+
+
+    loginError {
+          if (!checkUser) throw new Error('');
+
+    }
+    // serviceUnvailable = {
+    
+    // httpError = new HTTPError(
+    //     503,
+    //     'Sorry, service temporally unavailable',
+    //     (error as Error).message
+    // );
+    // return http}
+}
+
+export class ResourcesErrorController {
+
+
+    register {
+        if(req.body.email)
+        //
+
+    }
+
+
+    loginError {
+          if (!checkUser) throw new Error('');
+
+    }
+    // serviceUnvailable = {
+    
+    // httpError = new HTTPError(
+    //     503,
+    //     'Sorry, service temporally unavailable',
+    //     (error as Error).message
+    // );
+    // return http}
+}
